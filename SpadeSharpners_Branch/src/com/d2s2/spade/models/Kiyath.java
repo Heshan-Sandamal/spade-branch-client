fc8aad541a6956845f388e42007ef97e76ff83d3
Heshan-Sandamal <mhsandamal1@gmail.com>
2015-12-28 17:03:26 +0530

    Kiyath model added

---
+++ SpadeSharpners_Branch/src/com/d2s2/spade/models/Kiyath.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.d2s2.spade.models;
+
+/**
+ *
+ * @author Heshan Sandamal
+ */
+public class Kiyath{
+    
+    private String code;
+    private  String size;
+    private int noOfTips;
+
+    /**
+     * @return the code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * @return the size
+     */
+    public String getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the noOfTips
+     */
+    public int getNoOfTips() {
+        return noOfTips;
+    }
+
+    /**
+     * @param noOfTips the noOfTips to set
+     */
+    public void setNoOfTips(int noOfTips) {
+        this.noOfTips = noOfTips;
+    }
+    
+    
+}
