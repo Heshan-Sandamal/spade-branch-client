9a568afd2eef735d45b1c469d9f89ac3206596ca
Tharindu-Diluksha <tdiluksha@gmail.com>
2016-01-07 21:32:45 +0530

    DBconnection change

--- SpadeSharpners_Branch/src/com/d2s2/spade/dbconnection/DBConnection.java
+++ SpadeSharpners_Branch/src/com/d2s2/spade/dbconnection/DBConnection.java
@@ -21,11 +21,9 @@ public class DBConnection {
        Class.forName("com.mysql.jdbc.Driver");
 
 
-       connection = DriverManager.getConnection("jdbc:mysql://localhost/spades", "root", null);
-
-       //connection = DriverManager.getConnection("jdbc:mysql://localhost/spadeBranchDB", "root", "diluksha");
+       //connection = DriverManager.getConnection("jdbc:mysql://localhost/spades", "root", null);
+       connection = DriverManager.getConnection("jdbc:mysql://localhost/spadeBranchDB", "root", "diluksha");
         
-
     }
 
     public static DBConnection getDBConnection() throws ClassNotFoundException, SQLException {
@@ -36,7 +34,6 @@ public class DBConnection {
     }
 
     
-
     public Connection getConnection() {
         return connection;
 
